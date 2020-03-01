7d04bebe09a8e979bc6ae9a7baeb4528dbe40a96
adem <ademclblr@gmail.com>
2020-03-01 11:45:21 -0700

    second day

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -4,13 +4,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // to start selenium script we needed
         // setup webdriver (browser driver) and create webdriver object
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver=new ChromeDriver();
         // in selenium, everything starts from webdriver interface
-        driver.get("http://google.com");
+        driver.get("http://google.com"); //open website
+
+        Thread.sleep(3000); //for demo wait 3 seconds
+
+        driver.close();// to close driver
+
 
     }
 }
